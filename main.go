2fb79168b6c357951e254af03764bfa581c049f4
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-09-27 01:17:53 +0900

    Refactor building params

--- main.go
+++ main.go
@@ -6,15 +6,8 @@ import (
 	"time"
 )
 
-func main() {
-	fmt.Println("Creating diary page started.")
-
-	env := GetEnv()
-	client := NewClient(env.apiToken)
-
-	now := time.Now()
-
-	params := QueryDatabaseParams{
+func buildQueryDatabaseParams(now time.Time) QueryDatabaseParams {
+	return QueryDatabaseParams{
 		Filter: QueryDatabaseFilter{
 			Property: "Date",
 			QueryDatabasePropertyFilter: QueryDatabasePropertyFilter{
@@ -24,21 +17,14 @@ func main() {
 			},
 		},
 	}
-	res, err := client.QueryDatabase(env.diaryDatabaseId, params)
-	if err != nil {
-		log.Fatalf("failed to queryDatabase: %v", err)
-	}
+}
 
-	if len(res.Results) > 0 {
-		fmt.Println("Today's diary page was already created.")
-		return
-	}
-
+func buildCreatePageParams(databaseId string, now time.Time) CreatePageParams {
 	jaWeekdays := [...]string{"日", "月", "火", "水", "木", "金", "土"}
-	cpParams := CreatePageParams{
+	return CreatePageParams{
 		Parent: CreatePageParent{
 			Type:       "database_id",
-			DatabaseId: env.diaryDatabaseId,
+			DatabaseId: databaseId,
 		},
 		Properties: map[string]CreatePageProperty{
 			"Name": {
@@ -57,6 +43,28 @@ func main() {
 			},
 		},
 	}
+}
+
+func main() {
+	fmt.Println("Creating diary page started.")
+
+	env := GetEnv()
+	client := NewClient(env.apiToken)
+
+	now := time.Now()
+
+	qdParams := buildQueryDatabaseParams(now)
+	res, err := client.QueryDatabase(env.diaryDatabaseId, qdParams)
+	if err != nil {
+		log.Fatalf("failed to queryDatabase: %v", err)
+	}
+
+	if len(res.Results) > 0 {
+		fmt.Println("Today's diary page was already created.")
+		return
+	}
+
+	cpParams := buildCreatePageParams(env.diaryDatabaseId, now)
 	if err := client.CreatePage(cpParams); err != nil {
 		log.Fatalf("failed to createPage: %v", err)
 	}
