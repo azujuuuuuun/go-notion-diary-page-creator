5a7053b50332fa14b42fad6a13306036c783d514
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-09-27 23:12:02 +0900

    Create diary struct

---
+++ diary.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Diary struct {
+	databaseId string
+	client     *Client
+}
+
+func NewDiary(databaseId string, client *Client) *Diary {
+	return &Diary{databaseId: databaseId, client: client}
+}
+
+func (d *Diary) hasTodaysPage(now time.Time) (bool, error) {
+	params := QueryDatabaseParams{
+		Filter: QueryDatabaseFilter{
+			Property: "Date",
+			QueryDatabasePropertyFilter: QueryDatabasePropertyFilter{
+				Date: DateFilterCondition{
+					Equals: now.Format("2006-01-02"),
+				},
+			},
+		},
+	}
+
+	res, err := d.client.QueryDatabase(d.databaseId, params)
+	if err != nil {
+		return false, fmt.Errorf("failed to QueryDatabase: %v", err)
+	}
+
+	if len(res.Results) > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (d *Diary) createTodaysPage(now time.Time) error {
+	jaWeekdays := [...]string{"日", "月", "火", "水", "木", "金", "土"}
+	params := CreatePageParams{
+		Parent: CreatePageParent{
+			Type:       "database_id",
+			DatabaseId: d.databaseId,
+		},
+		Properties: map[string]CreatePageProperty{
+			"Name": {
+				Title: []TitleProperty{
+					{
+						Text: TextProperty{
+							Content: now.Format("2006/01/02") + "(" + jaWeekdays[now.Weekday()] + ")",
+						},
+					},
+				},
+			},
+			"Date": {
+				Date: &DateProperty{
+					Start: now.Format("2006-01-02"),
+				},
+			},
+		},
+	}
+
+	err := d.client.CreatePage(params)
+	if err != nil {
+		return fmt.Errorf("failed to CreatePage: %v", err)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -6,67 +6,26 @@ import (
 	"time"
 )
 
-func buildQueryDatabaseParams(now time.Time) QueryDatabaseParams {
-	return QueryDatabaseParams{
-		Filter: QueryDatabaseFilter{
-			Property: "Date",
-			QueryDatabasePropertyFilter: QueryDatabasePropertyFilter{
-				Date: DateFilterCondition{
-					Equals: now.Format("2006-01-02"),
-				},
-			},
-		},
-	}
-}
-
-func buildCreatePageParams(databaseId string, now time.Time) CreatePageParams {
-	jaWeekdays := [...]string{"日", "月", "火", "水", "木", "金", "土"}
-	return CreatePageParams{
-		Parent: CreatePageParent{
-			Type:       "database_id",
-			DatabaseId: databaseId,
-		},
-		Properties: map[string]CreatePageProperty{
-			"Name": {
-				Title: []TitleProperty{
-					{
-						Text: TextProperty{
-							Content: now.Format("2006/01/02") + "(" + jaWeekdays[now.Weekday()] + ")",
-						},
-					},
-				},
-			},
-			"Date": {
-				Date: &DateProperty{
-					Start: now.Format("2006-01-02"),
-				},
-			},
-		},
-	}
-}
-
 func main() {
 	fmt.Println("Creating diary page started.")
 
 	env := GetEnv()
 	client := NewClient(env.apiToken)
+	diary := NewDiary(env.diaryDatabaseId, client)
 
 	now := time.Now()
 
-	qdParams := buildQueryDatabaseParams(now)
-	res, err := client.QueryDatabase(env.diaryDatabaseId, qdParams)
+	b, err := diary.hasTodaysPage(now)
 	if err != nil {
-		log.Fatalf("failed to queryDatabase: %v", err)
+		log.Fatalf("failed to hasTodaysPage: %v", err)
 	}
-
-	if len(res.Results) > 0 {
+	if b {
 		fmt.Println("Today's diary page was already created.")
 		return
 	}
 
-	cpParams := buildCreatePageParams(env.diaryDatabaseId, now)
-	if err := client.CreatePage(cpParams); err != nil {
-		log.Fatalf("failed to createPage: %v", err)
+	if err := diary.createTodaysPage(now); err != nil {
+		log.Fatalf("failed to createTodaysPage: %v", err)
 	}
 
 	fmt.Println("Today's diary page was created successfully.")
